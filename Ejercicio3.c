ca4f6db1a28a237792bbb3e6d2560a5589214f1b
Fabio Pillajo <114030717+KineticBlaze23@users.noreply.github.com>
2025-11-17 15:15:47 -0500

    Refactor thread creation and timing for multiple scenarios
    Updated thread creation logic to handle 1, 2, and 3 million threads, measuring execution time.

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -2,24 +2,52 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <pthread.h>
-struct timeval tinicio , tfin ;
+
+struct timeval tinicio, tfin;
 double promedio = 0.0;
-void * hilo ( void * arg )
+
+void *hilo(void *arg)
 {
- gettimeofday (& tfin , NULL ) ;
- int tfin_seg= tfin.tv_sec;
- int tini_seg = tinicio.tv_sec;
- promedio += ( tfin_seg - tini_seg);
+    gettimeofday(&tfin, NULL);
+    long diff = (tfin.tv_sec - tinicio.tv_sec) * 1000000L +
+                (tfin.tv_usec - tinicio.tv_usec);
+    promedio += diff;
+    pthread_exit(NULL);
 }
-int main ()
+
+int main()
 {
- int i = 0;
- pthread_t h ;
- for ( i = 0 ; i < 100000 ; i ++ )
- {
- gettimeofday (& tinicio , NULL );
- pthread_create (& h, NULL , hilo , NULL) ;
- pthread_join (h, NULL);
- }
-printf ("El tiempo de creación de los %d hilos es %f segundos\n", i , promedio);
+    pthread_t h;
+    long i;
+    //1 MILLÓN DE HILOS 
+    promedio = 0.0;
+    for (i = 0; i < 1000000; i++)
+    {
+        gettimeofday(&tinicio, NULL);
+        pthread_create(&h, NULL, hilo, NULL);
+        pthread_join(h, NULL);
+    }
+    printf("Tiempo total para 1,000,000 hilos: %.0f microsegundos\n", promedio);
+
+    //2 MILLONES DE HILOS 
+    promedio = 0.0;
+    for (i = 0; i < 2000000; i++)
+    {
+        gettimeofday(&tinicio, NULL);
+        pthread_create(&h, NULL, hilo, NULL);
+        pthread_join(h, NULL);
+    }
+    printf("Tiempo total para 2,000,000 hilos: %.0f microsegundos\n", promedio);
+
+    // 3 MILLONES DE HILOS 
+    promedio = 0.0;
+    for (i = 0; i < 3000000; i++)
+    {
+        gettimeofday(&tinicio, NULL);
+        pthread_create(&h, NULL, hilo, NULL);
+        pthread_join(h, NULL);
+    }
+    printf("Tiempo total para 3,000,000 hilos: %.0f microsegundos\n", promedio);
+
+    return 0;
 }
